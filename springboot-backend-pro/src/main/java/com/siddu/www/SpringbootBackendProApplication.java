0fd0866cd705ad9b4eb1816d95af7a0848550ca0
K.S.vinay <22P31F0023@acet.ac.in>
2025-01-31 18:00:48 +0530

    my backend

---
+++ springboot-backend-pro/src/main/java/com/siddu/www/SpringbootBackendProApplication.java
@@ -0,0 +1,13 @@
+package com.siddu.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendProApplication.class, args);
+	}
+
+}
---
+++ springboot-backend-pro/src/main/java/com/siddu/www/service/EmployeeService.java
@@ -0,0 +1,58 @@
+package com.siddu.www.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.siddu.www.exception.ResourceNotFoundException;
+import com.siddu.www.model.Employee;
+import com.siddu.www.repo.EmployeeRepository;
+
+@Service
+public class EmployeeService 
+{
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	public List<Employee> getAllEmployees()
+	{
+		return employeeRepository.findAll();
+	}
+	
+	public Employee addEmployee(Employee employee)
+	{
+		return employeeRepository.save(employee);
+	}
+	
+
+	public Employee getEmployeeById(long id)
+	{
+		return employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Id Not Found"));
+	}
+	
+	public ResponseEntity<Employee> updateEmployeeById(long id,Employee employee)
+	{
+		System.out.println(employee);
+		Employee oldEmp=employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Id Not Found"));
+		
+		oldEmp.setFirstName(employee.getFirstName());
+		oldEmp.setLastName(employee.getLastName());
+		oldEmp.setEmail(employee.getEmail());
+		
+		Employee updateEmp=employeeRepository.save(oldEmp);
+		
+		return ResponseEntity.ok(updateEmp);
+	}
+	
+	public ResponseEntity<HttpStatus> deleteEmployee(long id)
+	{
+		Employee employee=employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee Does not Exit"));
+		employeeRepository.delete(employee);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+}
